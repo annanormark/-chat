29368d79071fac7cb1e194e738d50b025c05a7d5
annanormark <anna.nor94@gmail.com>
2015-12-11 16:52:11 +0100

    Userclass contain more

--- User.java
+++ User.java
@@ -11,6 +11,7 @@ public class User {
 		this.ip = ip;
 		messageLog = new MessageLog();
 		userList = new User[20];
+		
 	}
 
 	public void setName(String name) {
@@ -36,4 +37,31 @@ public class User {
     public void setMsgLog(MessageLog m){
 	this.messageLog = m;
     }
-}
+
+    public void sendMsg(String msg){
+
+    }
+
+    public String getMsg(){
+
+    }
+
+    public static void socketConnect(String ip) {
+	String serverName = ip;
+	int port = 6066;// Integer.parseInt(args[1]);
+	try {
+	    System.out.println("Connecting to " + serverName + " on port " + port);
+	    Socket client = new Socket(serverName, port);
+	    System.out.println("Just connected to " + client.getRemoteSocketAddress());
+	    OutputStream outToServer = client.getOutputStream();
+	    DataOutputStream out = new DataOutputStream(outToServer);
+	    out.writeUTF("Hello from " + client.getLocalSocketAddress());
+	    InputStream inFromServer = client.getInputStream();
+	    DataInputStream in = new DataInputStream(inFromServer);
+	    System.out.println("Server says " + in.readUTF());
+	    client.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+}§
